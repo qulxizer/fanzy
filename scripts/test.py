edf591c9c58a6458bcf836a8b9497438f189f481
qulx <qulxizer@gmail.com>
2026-07-11 01:28:23 +0300

    update: almost pingpong ig

--- scripts/test.py
+++ scripts/test.py
@@ -6,11 +6,17 @@ with serial.Serial("/dev/ttyACM0", 115200, timeout=1) as ser:
     ser.reset_input_buffer()
 
     while True:
-        ser.write(b"C")
+        ser.write(b"A")
         ser.flush()
-        print("TX: C")
+        byte = ser.read(1)
 
-        data = ser.read(1)
-        print("RX:", data, "hex=" + data.hex() if data else "timeout")
+        if not byte:
+            print("timeout")
+            break
 
+        print("received:", byte)
+
+        if byte == b"Z":
+            print("STM32 reply received")
+            break
         time.sleep(1)
